ba448761f419f26d81e64b4100978addd19db215
manuyuuu <manuyuuu@icloud.com>
2025-02-28 20:08:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our', 'his', 'her'];
+let adj = ['great', 'big', 'scary', 'weird'];
+let noun = ['jogger', 'racoon', 'bird', 'being', 'firewoman'];
+
+function generateDomains() {
+  let domains = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+          domains.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return domains;
+}
+
+function generateRandomDomain() {
+  let domains = generateDomains();
+  let randomIndex = Math.floor(Math.random() * domains.length);
+  return domains[randomIndex];
+}
+
+function updateDomain() {
+  document.getElementById("domain").innerText = generateRandomDomain();
+}
+
+window.onload = function () {
+  updateDomain();
+  document.getElementById("btn").addEventListener("click", updateDomain);
 };
